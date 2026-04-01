b760c0da34d594d0c8672a3b25caf4977daed2e8
RAJ6MAURYA <maurya62raj@gmail.com>
2026-04-01 23:14:54 +0530

    adding a todo

---
+++ TODO.md
@@ -0,0 +1,9 @@
+1. client module to init a AI client - DONE
+2. function to generate the data 
+3. function to view the data in a pretty format
+4. add Gradio UI for interactive session
+
+
+NOTE: Implement logger, context
+
+future upgrade: Allow important details of the user to be saved locally, which can be used to send it along with the prompt.
--- modules/backendAI/model.py
+++ modules/backendAI/model.py
@@ -1,13 +1,3 @@
-'''
-1. client module to init a AI client - DONE
-2. function to generate the data 
-3. function to view the data in a pretty format
-
-
-NOTE: Implement logger, context
-
-future upgrade: Allow important details of the user to be saved locally, which can be used to send it along with the prompt.
-'''
 
 from . import API_KEY, LOCAL_LLM, MODEL
 import openai
