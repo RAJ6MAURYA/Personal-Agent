e1ce7a50ee70d05aa88dfcc5a17a2d2d8d22124f
RAJ6MAURYA <maurya62raj@gmail.com>
2026-04-05 18:34:36 +0530

    adding streaming to the model

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ from modules.backendAI import model
 
 
 client = model.LLM()
-print(client.chatWithContext(userInput='hi i am raj maurya',conversationID="2"))
-print(client.chatWithContext(userInput='what\'s my name',conversationID="2"))
+
+for chunk in client.chat(userInput='hi i am raj maurya', conversationID="2"):
+	print(chunk,flush=True, end="")
+
+for chunk in client.chat(userInput='what\'s my name', conversationID="2"):
+	print(chunk,flush=True, end="")
+
--- modules/backendAI/model.py
+++ modules/backendAI/model.py
@@ -1,30 +1,43 @@
 
 from . import API_KEY, LOCAL_LLM, MODEL
 import openai
+import pprint
+
 
 class LLM:
     def __init__(self):
-        self._client = openai.OpenAI(base_url=LOCAL_LLM,api_key=API_KEY)
+        self._client = openai.OpenAI(base_url=LOCAL_LLM, api_key=API_KEY)
         self._conversations = {}
         self._behave = "Be nice and well spoken"
 
     # expects the instance of client and a prompt
-    def chatWithNoContext(self, instructions, userInput):
-        response = self._client.responses.create(model=MODEL,instructions=self._behave,input=userInput)
-        return response.output_text
-    
-    def setBehaviour(self,behave):
+
+    def setBehaviour(self, behave):
         self._behaviour = behave
 
-    def chatWithContext(self, userInput, conversationID):
+    def chat(self, userInput, conversationID):
+        # add the unique conversationID to the cache it it's a new chat
         if conversationID not in self._conversations:
             self._conversations[conversationID] = []
-        self._conversations[conversationID].append({"role": "user", "content": userInput})
-        response = self._client.responses.create(model=MODEL,instructions=self._behave,input=self._conversations[conversationID])
-        self._conversations[conversationID].append({"role": "assistant", "content": response.output_text})
-        return response.output_text
 
+        # cache the userInput
+        self._conversations[conversationID].append(
+            {"role": "user", "content": userInput})
 
-    
+        # Pass the conversation to the LLM
+        response = self._client.responses.create(
+            model=MODEL,
+            instructions=self._behave,
+            input=self._conversations[conversationID],
+            stream=True)
 
-
+        for event in response:
+            if event.type == "response.output_text.delta":
+                yield event.delta
+        
+        # full response
+        output = event.response.output[0].content[0].text
+        
+        # cache the LLM response
+        self._conversations[conversationID].append(
+            {"role": "assistant", "content": output})
