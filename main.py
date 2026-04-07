37af6e84d9f8b8afb9872bf1028a54b8e0da1106
RAJ6MAURYA <maurya62raj@gmail.com>
2026-04-07 23:33:55 +0530

    minor fix

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ prompt = input("enter a Prompt: ")
 while prompt != "exit":
 	for chunk in client.chat(userInput=prompt, conversationID="2"):
 		print(chunk,flush=True, end="")
-	print()
-	print()
+	print(end="\n \n")
 	prompt = input("Prompt: ")
 
 
