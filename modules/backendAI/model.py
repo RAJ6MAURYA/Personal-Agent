c92199312583aa6ca71c6f7d378d77f99fb8a092
RAJ6MAURYA <maurya62raj@gmail.com>
2026-04-09 23:42:10 +0530

    adding a LLM to summarise the output if the token goes beyond a certain threashold.

--- modules/backendAI/model.py
+++ modules/backendAI/model.py
@@ -2,13 +2,27 @@
 from . import API_KEY, LOCAL_LLM, MODEL
 import openai
 
+class Summary:
+    def __init__(self):
+        self._client = openai.OpenAI(base_url=LOCAL_LLM, api_key=API_KEY)
+        self._behave = "keep Crutial Information and Summarise"
+
+    
+    def summary(self,input):
+        response = self._client.responses.create(
+            model=MODEL,
+            instructions=self._behave,
+            input=input
+        )
+        output = response.output_text
+        return output
+
 class LLM:
+    summariser = Summary()
     def __init__(self):
         self._client = openai.OpenAI(base_url=LOCAL_LLM, api_key=API_KEY)
         self._conversations = {}
         self._behave = "Be nice and talk on point"
-
-    # expects the instance of client and a prompt
 
     def setBehaviour(self, behave):
         self._behave = behave
@@ -35,7 +49,11 @@ class LLM:
         
         # full response
         output = event.response.output[0].content[0].text
-        
-        # cache the LLM response
-        self._conversations[conversationID].append(
-            {"role": "assistant", "content": output})
+        if len(output) > 1000:
+            # cache the LLM response
+            self._conversations[conversationID].append(
+                {"role": "assistant", "content": self.summariser.summary(output)})
+        else:
+            # cache the LLM response
+            self._conversations[conversationID].append(
+                {"role": "assistant", "content": output})
