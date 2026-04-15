641a791b37a054849bd74abb9a61e6be23e68e9a
RAJ6MAURYA <maurya62raj@gmail.com>
2026-04-15 23:55:50 +0530

    removing stream for now

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ client = model.LLM()
 
 prompt = input("enter a Prompt: ")
 while prompt != "exit":
-	for chunk in client.chat(userInput=prompt, conversationID="2"):
-		print(chunk,flush=True, end="")
+	response = client.chat(message=prompt)
+	print(response)
 	print(end="\n \n")
 	prompt = input("Prompt: ")
 
